1293f65f9d9fe637e77c1d764389795834b8744e
Homo Efficio <homo.efficio@gmail.com>
2021-05-08 19:26:08 +0900

    쿼리 모델 추가

---
+++ query/src/main/kotlin/me/zepeto/creator/in_app_game/creator/query/QueryApplication.kt
@@ -0,0 +1,11 @@
+package me.zepeto.creator.in_app_game.creator.query
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QueryApplication
+
+fun main(args: Array<String>) {
+    runApplication<QueryApplication>(*args)
+}
---
+++ query/src/test/kotlin/me/zepeto/creator/in_app_game/creator/query/QueryApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.zepeto.creator.in_app_game.creator.query
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QueryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
