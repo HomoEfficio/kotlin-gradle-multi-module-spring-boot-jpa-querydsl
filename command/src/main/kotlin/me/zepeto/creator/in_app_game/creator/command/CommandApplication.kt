a20d9aabce0f07c76264db035d80ff3d2c252145
Homo Efficio <homo.efficio@gmail.com>
2021-05-08 14:44:11 +0900

    커맨드 모듈에 EntityScan 추가

--- command/src/main/kotlin/me/zepeto/creator/in_app_game/creator/command/CommandApplication.kt
+++ command/src/main/kotlin/me/zepeto/creator/in_app_game/creator/command/CommandApplication.kt
@@ -1,8 +1,10 @@
 package me.zepeto.creator.in_app_game.creator.command
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 
+@EntityScan(basePackages = ["me.zepeto.creator.in_app_game.creator.domain"])
 @SpringBootApplication
 class CommandApplication
 
