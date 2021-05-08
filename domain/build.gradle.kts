728e0612a52a7cf752c7d6e4f017d1c1d7a83616
Homo Efficio <homo.efficio@gmail.com>
2021-05-08 22:53:25 +0900

    querydsl 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,10 +1,13 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("me.zepeto.creator.in_app_game.creator.kotlin-library-conventions")
     id("org.springframework.boot") version "2.4.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("com.ewerk.gradle.plugins.querydsl") version "1.0.10"
 
+    kotlin("kapt")
     kotlin("plugin.spring") version "1.4.32"
     kotlin("plugin.jpa") version "1.4.32"
     kotlin("plugin.allopen") version "1.4.32"
@@ -30,6 +33,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.querydsl:querydsl-jpa")
+    kapt("com.querydsl:querydsl-apt:4.4.0:jpa")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
@@ -58,4 +63,16 @@ allOpen {
     annotation("javax.persistence.Entity")
     annotation("javax.persistence.MappedSuperclass")
     annotation("javax.persistence.Embeddable")
+}
+
+val generatedQuerydslDir = "$buildDir/generated/source/kapt/main"
+
+querydsl {
+    library = "com.querydsl:querydsl-apt"
+    jpa = true
+    querydslSourcesDir = generatedQuerydslDir
+}
+
+sourceSets["main"].withConvention(KotlinSourceSet::class) {
+    kotlin.srcDir(generatedQuerydslDir)
 }
