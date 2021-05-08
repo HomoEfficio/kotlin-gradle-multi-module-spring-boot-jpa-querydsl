5a4e0847462e705a1ec15cd1bfc4775621272f67
Homo Efficio <homo.efficio@gmail.com>
2021-05-09 00:22:21 +0900

    쿼리 모듈에 EntityScan, EnableJpaRepositories 추가

--- query/src/main/kotlin/me/zepeto/creator/in_app_game/creator/query/QueryApplication.kt
+++ query/src/main/kotlin/me/zepeto/creator/in_app_game/creator/query/QueryApplication.kt
@@ -1,8 +1,12 @@
 package me.zepeto.creator.in_app_game.creator.query
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+@EntityScan(basePackages = ["me.zepeto.creator.in_app_game.creator.domain"])
+@EnableJpaRepositories(basePackages = ["me.zepeto.creator.in_app_game.creator.domain"])
 @SpringBootApplication
 class QueryApplication
 
