d1ee167293d3d9a58f229b08e2bf8da8980d3b33
Homo Efficio <homo.efficio@gmail.com>
2021-05-08 19:00:40 +0900

    P6Spy 추가 및 설정

--- command/src/test/kotlin/me/zepeto/creator/in_app_game/creator/command/service/WorldServiceTest.kt
+++ command/src/test/kotlin/me/zepeto/creator/in_app_game/creator/command/service/WorldServiceTest.kt
@@ -4,11 +4,13 @@ import me.zepeto.creator.in_app_game.creator.domain.World
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.context.annotation.Import
 
 @DataJpaTest
 @Import(WorldService::class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 internal class WorldServiceTest {
 
     @Autowired
